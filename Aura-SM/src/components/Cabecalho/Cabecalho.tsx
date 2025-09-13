304acdd6cf6d26315235c145a0dc46dc34ec915f
Jounaxis <jgs123098@gmail.com>
2025-09-13 12:47:55 -0300

    Criação dos componentes

---
+++ Aura-SM/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,10 @@
+import Menu from "../Menu/Menu";
+
+export default function Cabecalho() {
+    return (
+        <header>
+            <h1>Sistema de produtos</h1>
+            <Menu />
+        </header>
+    );
+}
